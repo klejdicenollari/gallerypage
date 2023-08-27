f4aa09aab56ce40ca4a142afa4c4caa303800141
klejdicenollari <klejdi244@gmail.com>
2023-08-27 10:19:55 +0200

    push

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
 </Routes>
 
 <footer className="footer">
-      <a href="#" className="link">Template</a> <p>created by</p> <a href="#" className="link"> Klejdi Cenollari </a>
+      <a href="#" className="link">Template</a> <p>created by</p> <a href="https://klejdicenollari.netlify.app/" className="link"> Klejdi Cenollari </a>
       </footer>
 
     </div>
